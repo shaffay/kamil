2e62415922defaf0eb2bb57966e9faebc9b72bd3
Shaffay Bajwa <Bajwa@aptechtr.edu>
2021-11-05 12:09:31 +0500

    bajwa here

---
+++ session1.php
@@ -0,0 +1,52 @@
+<h2>
+<?php
+$name = "shaffaybajwa";
+
+$position = "Faculty 20";
+
+
+$a = 10;
+
+$b = 20;
+
+echo "<br>";
+echo "<br>";
+
+echo $a,$b;
+
+echo "<br>";
+
+echo $a." ".$b;
+
+echo "<br>";
+
+echo "The value of a :".$a." & The Value of b :".$b;
+
+echo "<br>";
+
+echo $a + $b;
+
+echo "<br>";
+
+$a = 30;
+
+$c = $a + $b;
+
+echo $c;
+
+echo "<br>";
+echo "<br>";
+
+echo  $name." Is Your ".$position;
+echo "<br>";
+echo  $name." Is Your Faculty";
+?>
+</h2>
+
+<br>
+
+<?php
+
+echo  "Hello World";
+
+?>
